cc670f51fc87c55e816ba84af9fcc4e71b2239b6
Nezaale <ahmadnezal19@gmail.com>
2025-11-25 15:18:51 +0530

    added favicon

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold">Welcome to uniSphere</h1>
-      <p className="text-gray-600 mt-2">
+    <>
+    <div 
+      className="h-[calc(100vh-64px)] bg-cover bg-center border-radius-lg flex items-center justify-center " 
+      style={{ backgroundImage: "url('https://gulms.galgotiasuniversity.org/pluginfile.php/1/theme_boost/loginbackgroundimage/1759992477/WhatsApp%20Image%202025-06-24%20at%2010.19.29%20AM.jpeg')" }}
+      >
+
+
+    <div className="p-6" >
+      <h1 className="text-7xl font-bold text-white">Welcome to uniSphere</h1>
+      <p className="text-white mt-2">
+        
         Explore and register for university events.
       </p>
     </div>
+      </div>
+
+      </>
   );
 }
